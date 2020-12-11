b3c732fd11fcb95f31674fc4e02b5edba58cb0b2
Pedro Leite <pedroaug.fl@gmail.com>
2020-12-11 16:06:15 -0300

    Add files via upload

--- instacart.py
+++ instacart.py
@@ -16,7 +16,7 @@ orders = pd.read_csv('orders.csv')
 products = pd.read_csv('products.csv')
 sample_submission = pd.read_csv('sample_submission.csv')
 
-#Data exploration
+#1) Data exploration
 orders.columns #seems to be good for exploring the time in which purchases were made
 orders.dtypes
 #Day of the week
@@ -50,10 +50,36 @@ order3 = order1.merge(order2, how='inner')
 
 order3['product_name'].value_counts().head(10).plot(
     kind='barh', figsize=(20,10), fontsize=12)
-plt.title('Products that are mostly put first into the masket', fontsize = 20)
+plt.title('Most sold products', fontsize = 20)
 plt.ylabel('Product')
 plt.xlabel('Number')
 plt.show()
 
+#2) Data preparation
+#Already cleaned, but we will modify it to our purposes
+#Know what was bought in each order, in which order it was added to the cart
+order3 = order3.sort_values(by=['order_id','add_to_cart_order'])
+order4 = order3.groupby('order_id')['product_name'].agg(', '.join).reset_index()
 
+#3) Apriori algorithm
+from mlxtend.frequent_patterns import association_rules, apriori
+from mlxtend.preprocessing import TransactionEncoder
 
+# get all shopping lists as one list
+one_product = list(order4['product_name'].apply(lambda x: sorted(x.split(','))))
+
+# instantiate transcation encoder
+encoder = TransactionEncoder().fit(one_product)
+onehot = encoder.transform(one_product)
+
+# convert one-hot encode data to DataFrame
+onehot = pd.DataFrame(onehot, columns=encoder.columns_)
+# compute frequent items using the Apriori algorithm - Get up to three items
+frequent_itemsets = apriori(onehot, min_support=0.001, max_len=3, use_colnames=True)
+
+# compute all association rules for frequent_itemsets
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+
+# given that the left-hand side has two items, then which item is more likely to be added to the basket?
+rules['lhs items'] = rules['antecedents'].apply(lambda x:len(x) )
+rules[rules['lhs items']>1].sort_values('lift', ascending=False).head()
